efe8d1d67472e9c294a296a94522caff6f4b9923
BenjaminChan <benjamin.v.chan@gmail.com>
2025-04-19 23:58:41 -0700

    04 script outline

---
+++ scripts/04_ml_modeling.py
@@ -0,0 +1,6 @@
+# 1. Load processed data
+# 2. Define X (all numeric features except target) and y = Per_Capita_Sugar_Consumption
+# 3. Split train/test (e.g. 80/20 random)
+# 4. Train RandomForestRegressor
+# 5. Evaluate RMSE & R2, save metrics to outputs/models/metrics.txt
+# 6. Save trained model to outputs/models/rf_model.pkl
